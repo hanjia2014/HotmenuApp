e94b4bf7277677c3d33870f6f9a27050b10925c6
hanjia2014 <hanj@datacom.co.nz>
2015-07-24 16:02:05 +1200

    order hub

--- src/Hotmenu2015/Hub/OrderHub.cs
+++ src/Hotmenu2015/Hub/OrderHub.cs
@@ -2,7 +2,16 @@
 
 namespace HotmenuApp.Hub
 {
-    public class OrderHub : Microsoft.AspNet.SignalR.Hub
+    public class OrderHub : Microsoft.AspNet.SignalR.Hub<IOrderHub>
     {
+        public void Send(string name, string message)
+        {
+            Clients.All.AddNewMessageToPage(name, message);
+        }
+    }
+
+    public interface IOrderHub
+    {
+        void AddNewMessageToPage(string key, string value);
     }
 }
