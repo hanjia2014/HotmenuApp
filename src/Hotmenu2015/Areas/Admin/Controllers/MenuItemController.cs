0325a15f6d877d35a056d7999e780f8a6a6c6dbe
hanjia2014 <hanj@datacom.co.nz>
2015-09-11 08:42:19 +1200

    menu item edit page

--- src/Hotmenu2015/Areas/Admin/Controllers/MenuItemController.cs
+++ src/Hotmenu2015/Areas/Admin/Controllers/MenuItemController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using HotmenuApp.Models;
+using Microsoft.AspNet.Mvc.Rendering;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -24,6 +25,12 @@ namespace HotmenuApp.Areas.Admin.Controllers
             {
                 return HttpNotFound();
             }
+
+            var categories = new List<SelectListItem>();
+            DbContext.Categories.ToList().ForEach(p => categories.Add(new SelectListItem { Text = p.Name, Value = p.Id.ToString(), Selected = p.Id == item.CategoryId }));
+
+            ViewBag.Categories = categories;
+
             return View(item);
         }
     }
