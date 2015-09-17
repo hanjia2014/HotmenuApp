72deb55c2e1c35166c5c0b007869a2383271e80a
hanjia2014 <hanj@datacom.co.nz>
2015-09-18 09:27:11 +1200

    add generate new string guid

--- src/Hotmenu2015/wwwroot/app/models/Guid.js
+++ src/Hotmenu2015/wwwroot/app/models/Guid.js
@@ -36,6 +36,13 @@ var Guid = (function () {
     Guid.prototype.valueOf = function () {
         return this.id;
     };
+    Guid.generateNewId = function () {
+        var guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+        return guid;
+    };
     Guid.emptyGuid = new Guid("00000000-0000-0000-0000-000000000000");
     return Guid;
 })();
